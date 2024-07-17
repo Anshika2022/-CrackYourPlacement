fc18769740c280ad186e9987ad045c06067d795e
ANSHIKA KUMARI <101940406+Anshika2022@users.noreply.github.com>
2024-07-17 22:28:21 +0530

    Create Two sum.java

---
+++ Two sum.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+
+       HashMap<Integer,Integer> map = new HashMap();
+
+       //Fill HM
+       for(int i=0;i<nums.length;i++){
+           map.put(nums[i],i);
+       } 
+
+
+       //Searching 
+       for(int i=0; i<nums.length; i++){//2 7 11 15 target = 4
+          int num =nums[i]; 
+          int rem =target - num; 
+          if(map.containsKey(rem)){
+              int index = map.get(rem);
+              if(index ==i)continue;
+              return new int[]{i,index};
+          }
+       } 
+
+       return new int[]{};
+    }
+}
