9c8db6a1627114492ee90ac9a2790db988861d14
ANSHIKA KUMARI <101940406+Anshika2022@users.noreply.github.com>
2024-07-23 20:53:50 +0530

    Create Find Pair Given Difference.java

---
+++ Find Pair Given Difference.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int findPair(int n, int x, int[] arr) {
+        // code here
+        Arrays.sort(arr);
+        int i =0;
+        int j = 1;
+        while(i<n && j<n)
+        {
+            if(i != j && arr[j]-arr[i] ==x)
+            return 1;
+            else if(arr[j]-arr[i]<x)
+            j++;
+            else
+            i++;
+        }
+        return -1;
+        
+    }
+}
