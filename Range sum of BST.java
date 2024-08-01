6740de4c740f7d54a9b959fe99116f1370e8e1c8
ANSHIKA KUMARI <101940406+Anshika2022@users.noreply.github.com>
2024-08-01 21:13:44 +0530

    Create Range sum of BST.java

---
+++ Range sum of BST.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int rangeSumBST(TreeNode root, int low, int high) {
+        int sum = 0;
+        if(root == null)return sum;
+        if(root.val >= low && root.val <= high)sum += root.val;
+        if(root.val < high) sum+= rangeSumBST(root.right, low,high);
+        if(root.val > low) sum+= rangeSumBST(root.left, low,high);
+         return sum;
+    }
+}
