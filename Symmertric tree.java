217a3c25aa6c06536ec7b63e3806d5e450061621
ANSHIKA KUMARI <101940406+Anshika2022@users.noreply.github.com>
2024-08-01 21:17:49 +0530

    Create Symmertric tree.java

---
+++ Symmertric tree.java
@@ -0,0 +1,36 @@
+class SymmetricTree {
+
+  // iterative method to determine if a binary tree is symmetric
+  // using 2 queues
+  boolean isSymmetric(TreeNode root) {
+
+      if (root == null) return true;
+
+      Queue<TreeNode> leftTree = new LinkedList<>();
+      Queue<TreeNode> rightTree = new LinkedList<>();
+
+      leftTree.add(root.left);
+      rightTree.add(root.right);
+
+      while (!leftTree.isEmpty() && !rightTree.isEmpty()) {
+
+        TreeNode leftNode = leftTree.poll();
+        TreeNode rightNode = rightTree.poll();
+
+        if (leftNode == null && rightNode == null) continue;
+
+        if (leftNode == null || rightNode == null) return false;
+
+        if (leftNode.val != rightNode.val) return false;
+
+        // Pushing order is very important
+        leftTree.add(leftNode.left);
+        leftTree.add(leftNode.right);
+        rightTree.add(rightNode.right);
+        rightTree.add(rightNode.left);
+      }
+
+      return true;
+  }
+
+}
