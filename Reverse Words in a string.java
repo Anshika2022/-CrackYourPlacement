18192bf788e1e2026fbbbb86dddb2a729026aa6f
ANSHIKA KUMARI <101940406+Anshika2022@users.noreply.github.com>
2024-08-05 16:56:07 +0530

    Reverse Words in a string.java

---
+++ Reverse Words in a string.java
@@ -0,0 +1,19 @@
+public String reverseWords(String s) {
+    String result = new String();
+    int i = 0;
+    int n = s.length();
+
+    while(i < n){
+        while(i < n && s.charAt(i) == ' ') i++;
+        if(i >= n) break;
+        int j = i + 1;
+        while(j < n && s.charAt(j) != ' ') j++;
+        String sub = s.substring(i, j);
+        if(result.length() == 0) result = sub;
+        else result = sub + " " + result;
+        i = j+1;
+    }
+    return result;
+  }
+}
+ 
